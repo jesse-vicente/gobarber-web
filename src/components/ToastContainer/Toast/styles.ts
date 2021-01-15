2cc122432f6b0d929319350ee6c709a514e6b679
Jessé Santos <jesse.vicente96@gmail.com>
2021-01-15 00:09:50 -0300

    Initial commit

---
+++ src/components/ToastContainer/Toast/styles.ts
@@ -0,0 +1,73 @@
+import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
+
+interface ToastProps {
+  type?: 'success' | 'error' | 'warning' | 'info';
+  hasDescription: boolean;
+}
+
+const toastTypeVariations = {
+  info: css`
+    background: #0086d6;
+  `,
+  success: css`
+    background: #43d787;
+  `,
+  error: css`
+    background: #f9461c;
+  `,
+  warning: css`
+    background: #ffbb00;
+  `,
+};
+
+export const Container = styled(animated.div) <ToastProps>`
+  width: 360px;
+  color: #fff;
+
+  position: relative;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) => toastTypeVariations[props.type || 'info']}
+
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 16px;
+    top: 19px;
+    opacity: 0.6;
+    border: 0;
+    background: transparent;
+    color: inherit;
+  }
+
+  ${(props) => !props.hasDescription && css`
+    align-items: center;
+
+    svg {
+      margin-top: 0;
+    }
+  `}
+`;
